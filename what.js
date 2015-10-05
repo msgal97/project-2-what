5ac77c6f02ddbd1f21e1b57d82133e867738dd0e
msgal97 <klyoung2@mail.bradley.edu>
2015-10-04 19:14:48 -0500

    js change

--- what.js
+++ what.js
@@ -1,5 +1,11 @@
 var textarea = document.getElementById('what');
-var form = document.getElementByTagName('form')[0];
+var form = document.getElementsByTagName('form')[0];
+var what = location.search;
+what = what.replace('?what=','');
+what = what.replace(/\+/g, ' ');
+what = decodeURIComponent(what);
+textarea.value = what;
+//console.log(what);
 
 textarea.addEventListener('change', function(){
   form.submit();
